8d28d3cfb263903c6d8cc74e1c0075c787ff7c04
Ifiezibe (sammyjayisthename) <87184553+thetruesammyjay@users.noreply.github.com>
2025-12-01 06:34:51 -0800

    dist

---
+++ packages/math/dist/funding.d.ts
@@ -0,0 +1,2 @@
+export declare function calculateFundingPayment(positionSize: number | string, markPrice: number | string, fundingRate: number | string): number;
+export declare function calculateFundingRate(markPrice: number | string, indexPrice: number | string, fundingIntervalHours?: number): number;
